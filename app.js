14455d03c3d4b513dd187b59605ec80c0119da59
sarshad259 <154915864+sarshad259@users.noreply.github.com>
2025-01-12 13:18:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req,res) => {
     res.send("hello world")
 })
 
+app.get("/vercel", (req,res) => {
+    res.send("you are on vercel")
+})
+
 app.listen(4000 , (req, res) => {
  console.log(`app running on port ${4000}`);
 })
